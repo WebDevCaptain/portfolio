5be217d14b41ccc463a823b7ac9a5d88ab6580e6
Amy Kirasack <amykirasack@gmail.com>
2019-01-02 21:20:12 +0100

    chore: Remove page-2, remove unnecessary links from index

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,14 +14,6 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <ul>
-      <li>
-        <Link to="/page-2/">Go to page 2</Link>
-      </li>
-      <li>
-        <Link to="/about/">About Me</Link>
-      </li>
-    </ul>
   </Layout>
 );
 

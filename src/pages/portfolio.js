19a324d4baf4491dcde11e1b4ee19b1b6d38760d
Amy Kirasack <amykirasack@gmail.com>
2019-01-02 21:30:38 +0100

    feat: Create project component

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
+import Project from '../components/project';
 
 const PortfolioPage = () => (
   <Layout>
     <h1>This is the portfolio!</h1>
     <p>Cool projects go here!</p>
+    <Project />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 );

71d180ae6c57b7634294bd66c65bab6fcb9d4852
Amy Kirasack <amykirasack@gmail.com>
2019-01-07 00:06:43 +0100

    style: Start styling header

--- src/components/header.js
+++ src/components/header.js
@@ -2,56 +2,81 @@ import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import gatsbyLogo from '../images/gatsby-icon.png';
+import styled from 'styled-components';
+
+const HeaderWrapper = styled.div`
+  background: #c9af98;
+  margin-bottom: 1rem;
+`;
+
+const Title = styled.h1`
+  position: absolute;
+  margin-left: 5vw;
+  margin-top: 2vh;
+`;
+
+const NavbarContainer = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0.5rem 1rem;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const NavList = styled.ul`
+  padding-top: 0.5rem;
+  display: flex;
+  flex-direction: row;
+  list-style-type: none;
+  font-size: 20px;
+`;
+
+const NavLi = styled.li`
+  padding: 0 20px;
+  color: #fafafa;
+  a:visited {
+    text-decoration: none;
+  }
+`;
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          <span>
-            {siteTitle}
-            <img
-              style={{ height: '30px' }}
-              src={gatsbyLogo}
-              alt="Gatsby Logo"
-            />
-          </span>
-        </Link>
-      </h1>
-      <ul>
-        <li>
-          <Link to="/about/">about</Link>
-        </li>
-        <li>
-          <Link to="/portfolio/">portfolio</Link>
-        </li>
-        <li>
-          <Link to="/contact/">contact</Link>
-        </li>
-        <li>
-          <Link to="/blog/">blog</Link>
-        </li>
-      </ul>
-    </div>
-  </div>
+  <HeaderWrapper>
+    <Title>
+      <Link
+        to="/"
+        style={{
+          color: `white`,
+          textDecoration: `none`,
+        }}
+      >
+        <span>{siteTitle}</span>
+      </Link>
+    </Title>
+    <NavbarContainer>
+      <NavList>
+        <NavLi>
+          <Link to="/about/" style={{ textDecoration: 'none' }}>
+            about
+          </Link>
+        </NavLi>
+        <NavLi>
+          <Link to="/portfolio/" style={{ textDecoration: 'none' }}>
+            portfolio
+          </Link>
+        </NavLi>
+        <NavLi>
+          <Link to="/contact/" style={{ textDecoration: 'none' }}>
+            contact
+          </Link>
+        </NavLi>
+        <NavLi>
+          <Link to="/blog/" style={{ textDecoration: 'none' }}>
+            blog
+          </Link>
+        </NavLi>
+      </NavList>
+    </NavbarContainer>
+  </HeaderWrapper>
 );
 
 Header.propTypes = {

bd10040825b16e8f4b07ebe656f2115378a252fb
Amy Kirasack <amykirasack@gmail.com>
2019-01-06 22:38:06 +0100

    build: Set up architecture for blog, blog posts

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+const Blog = () => (
+  <Layout>
+    <h1>I'm under construction at the moment</h1>
+    <em>Come back again later!</em>
+  </Layout>
+);
+
+export default Blog;
